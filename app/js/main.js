f7cc0da3957c2389a1348e145cfca075eac0eb78
Pixus20 <mihalcukvasa01@gmail.com>
2024-01-17 12:42:40 +0200

    rewrite JS. Remove some useles fragments. Remove inline style.

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,7 @@
 document.querySelector(".menu_burger").addEventListener("click", function () {
   document.querySelector(".menu_nav").classList.toggle("open");
 });
-
-document.querySelector(".menu_list").addEventListener("click", function (e) {
-  if (e.target.tagName === "LI") {
-    document.querySelectorAll(".menu_list li").forEach(function (el) {
-      el.classList.remove("active");
-    });
-    e.target.classList.add("active");
-  }
-});
-
+//form
 var form = document.getElementById("registration_form");
 var nameInput = document.getElementById("form_name");
 var surnameInput = document.getElementById("form_surname");
@@ -19,118 +10,63 @@ var numberInput = document.getElementById("form_number");
 var emailInput = document.getElementById("form_email");
 var checkboxInput = document.getElementById("Checkbox");
 var submitBtn = document.querySelector(".form_button");
-
-var formTitle1 = document.getElementById("form_title1");
-var formTitle2 = document.getElementById("form_title2");
-var formTitle3 = document.getElementById("form_title3");
-var formTitle4 = document.getElementById("form_title4");
-var formTitle5 = document.getElementById("form_title5");
-var formTitle6 = document.getElementById("form_title6");
-
 // submit form
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var isNameValid = formTitle1.style.color === "black";
-  var isSurnameValid = formTitle2.style.color === "black";
-  var isSelectValid = formTitle3.style.color === "black";
-  var isNumberValid = formTitle4.style.color === "black";
-  var isEmailValid = formTitle5.style.color === "black";
-  var isCheckboxValid = formTitle6.style.color === "black";
-
-  if (isNameValid && isSurnameValid && isSelectValid && isNumberValid && isEmailValid && isCheckboxValid) {
-    nameInput.value = "";
-    surnameInput.value = "";
-    selectElement.value = "";
-    numberInput.value = "";
-    emailInput.value = "";
-    checkboxInput.checked = false;
-
-    formTitle1.style.color = "";
-    formTitle2.style.color = "";
-    formTitle3.style.color = "";
-    formTitle4.style.color = "";
-    formTitle5.style.color = "";
-    formTitle6.style.color = "";
-    alert("Форма надіслана успішно!");
-  } else {
-    alert("Будь ласка, заповніть усі поля правильно!");
-  }
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Форма надіслана успішно!");
+  form.reset();
 });
-
 // name input
 nameInput.addEventListener("input", function (e) {
   var sanitizedValue = e.target.value.replace(/[^a-zA-Z\u0400-\u04FFёЁ']/g, "");
   sanitizedValue = sanitizedValue.slice(0, 20);
   e.target.value = sanitizedValue;
-  formTitle1.style.color = sanitizedValue.trim() !== "" ? "black" : "";
 });
-
 // surname input
 surnameInput.addEventListener("input", function (e) {
   var sanitizedValue = e.target.value.replace(/[^a-zA-Z\u0400-\u04FFёЁ']/g, "");
   sanitizedValue = sanitizedValue.slice(0, 30);
   e.target.value = sanitizedValue;
-  formTitle2.style.color = sanitizedValue.trim() !== "" ? "black" : "";
 });
-
 // select input
 selectElement.addEventListener("change", function () {
   var selectedOption = selectElement.options[selectElement.selectedIndex].value;
-  formTitle3.style.color = selectedOption === "developer" || selectedOption === "QA" ? "black" : "";
 });
 //number
 numberInput.addEventListener("input", function (e) {
   var sanitizedValue = e.target.value.replace(/[^0-9+]/g, "");
   sanitizedValue = sanitizedValue.slice(0, 13);
   e.target.value = sanitizedValue;
-  if (sanitizedValue.length === 13) {
-    formTitle4.style.color = "black";
-  } else {
-    formTitle4.style.color = "red";
-  }
 });
-
 numberInput.addEventListener("focus", function (e) {
   if (!e.target.value.startsWith("+38")) {
     e.target.value = "+38" + e.target.value;
-    formTitle4.style.color = "black";
   }
 });
-
 numberInput.addEventListener("blur", function (e) {
   var inputValue = e.target.value;
-  if (inputValue.length !== 13) {
-    formTitle4.style.color = "red";
-  }
   if (inputValue.length < 13) {
     alert("Введіть вірний номер!");
     numberInput.value = "";
   }
 });
-
 // email input
-emailInput.addEventListener("input", function (e) {
-  var sanitizedValue = "";
-  for (var i = 0; i < e.target.value.length; i++) {
-    var char = e.target.value[i];
-    if (i !== 0 || /[a-zA-Z0-9]/.test(char)) {
-      sanitizedValue += char.replace(/[^a-zA-Z0-9._@-]/g, "");
+emailInput.addEventListener("blur", function (e) {
+  var inputValue = e.target.value.trim();
+  if (inputValue) {
+    if (/[\u0400-\u04FF]/.test(inputValue)) {
+      alert("Введення кирилиці заборонено!");
+      e.target.value = "";
+      return;
     }
-  }
-  sanitizedValue = sanitizedValue.slice(0, 50);
-
-  var emailRegex = /^[a-zA-Z0-9][a-zA-Z0-9._%+-]*@[a-zA-Z0-9]+([.-]*[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
-  if (emailRegex.test(sanitizedValue)) {
-    e.target.value = sanitizedValue;
-    formTitle5.style.color = "black";
+    var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(inputValue)) {
+      alert("Введіть правильний Email!");
+      e.target.value = "";
+    }
   } else {
-    formTitle5.style.color = "";
+    alert("Поле Email не може бути порожнім!");
   }
-});
-
-// checkbox input
-checkboxInput.addEventListener("click", function () {
-  formTitle6.style.color = checkboxInput.checked ? "black" : "";
 });
 //slider second block
 $(".content_development").slick({
@@ -172,7 +108,6 @@ $(".content_fourth_slider, .content_six_slider ").slick({
   fade: true,
   cssEase: "ease-in-out",
 });
-
 //menu scroll with height header
 document.addEventListener("DOMContentLoaded", function () {
   const menuLinks = document.querySelectorAll("header a");
